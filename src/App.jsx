9027ce5189bda5edef524b43494dab5e8cc46acd
Nitin <nitinsharmaop828@gmail.com>
2026-01-10 00:41:14 +0530

    2 sections of hero

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ function App() {
   
 
   return (
-     <Hero></Hero>
+    
+   <Hero></Hero>
+     
   )
 }
 
--- src/components/Sections/Hero.jsx
+++ src/components/Sections/Hero.jsx
@@ -2,23 +2,41 @@ import React from 'react'
 
 const Hero = () => {
   return (
-      <div className='h-screen w-screen bg-[#1A1C21] flex justify-center items-center'>
-        <div className='flex w-full max-w-6xl h-3/4 gap-6 '>
-          {/* left section */}
-          
-        <div className='flex-1 items-center justify-center bg-amber-400'>
-          <div className='pl-12'>
-          <h1 className='text-6xl  font-bold'>"AI</h1>
-           <h1 className='text-6xl  font-bold'>that</h1>
-            <h1 className='text-6xl  font-bold'>Understands</h1>
-             <h1 className='text-6xl gap-0.5 font-bold'> Your</h1>
-              <h1 className='text-6xl gap-0.5 font-bold'>Career"</h1>
-              </div>
-        </div>
-        {/* right section */}
-        <div className='flex-1 bg-amber-950'></div>
+      <section className="min-h-screen bg-[#1A1C21] text-white flex items-center">
+      <div className="mx-auto w-full max-w-6xl px-6">
+        
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+
+          {/* LEFT PANEL */}
+          <div className="bg-amber-400 p-12 flex items-center rounded-xl">
+            <div className="leading-tight">
+              <h1 className="text-6xl md:text-7xl font-extrabold text-white">AI</h1>
+              <h1 className="text-5xl md:text-6xl font-bold text-white">That</h1>
+              <h1 className="text-5xl md:text-6xl font-bold text-white">Understands</h1>
+              <h1 className="text-5xl md:text-6xl font-bold text-white">Your</h1>
+              <h1 className="text-6xl md:text-7xl font-extrabold text-white">Career</h1>
+
+              <p className="mt-6 max-w-md text-white/90 text-lg">
+                Build structured, AI-optimized resumes that stand out to both
+                recruiters and ATS systems.
+              </p>
+
+              <button className="mt-12  rounded-xl bg-blue-400 px-8 py-4 text-lg font-semibold text-white hover:bg-black transition">
+                       Get Started
+              </button>
+            </div>
+          </div>
+
+          {/* RIGHT PANEL */}
+          <div className="hidden md:flex items-center justify-center bg-amber-950 rounded-xl">
+            <span className="text-white/60">
+              Resume Preview Area
+            </span>
+          </div>
+
         </div>
       </div>
+    </section>
   )
 }
 
